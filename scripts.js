91b5eb81b9fb21b4565f6dec618abda1793a72e4
F Sandoval <fransandoval093@gmail.com>
2021-04-17 00:14:25 -0700

    added script changes

--- scripts.js
+++ scripts.js
@@ -1,56 +1,24 @@
-// // Set the date we're counting down to
-// var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-
-//   // Get today's date and time
-//   var now = new Date().getTime();
-    
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-    
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-//   // Output the result in an element with id="demo"
-//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-    
-//   // If the count down is over, write some text 
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-
-// var start_button = document.querySelector("#start");
-// console.log(start_button)
-// // start_button.addEventListener("click",template);
-
-
 // Selects element by class
 var timeEl = document.querySelector(".time");
-
 // Selects element by id
 var mainEl = document.getElementById("main");
-
+var btnEL = document.getElementById('start');
 var secondsLeft = 10;
 
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
+
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
     if(secondsLeft === 0) {
+
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
       sendMessage();
+
     }
 
   }, 1000);
